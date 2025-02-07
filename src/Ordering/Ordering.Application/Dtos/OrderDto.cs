c1876a7775d9197e913808c7f9bdf511d71f6a0f
Jonathan Rodriguez <rjonathan@outlook.es>
2025-02-06 20:20:32 -0400

    Utilizando el enfoque de Minimal APIs junto con el framework Carter, que facilita la definición de rutas de una manera más estructurada.

---
+++ src/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Dtos
+{
+    public class OrderDto
+    {
+        public Guid CustomerID { get; set; } = default!;
+        public string CustomerName { get; set; } = default!;
+        public decimal TotalAmount { get; set; } = default!;
+    }
+}
